4ba63a4a28a0e1145ae6a221a45ad621fac75413
alex5 <angrymarshall@gmail.com>
2025-06-21 20:25:10 +0300

    feat() add new products

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,9 @@
-import products from "./fixtures/category.json" with { type: 'json' };
+import category from "./fixtures/category.json" with {type: 'json'};
 
-export type Product = (typeof products)[number];
+export type Product = (typeof category)['products'][number];
 
 export type CartItem = { product: Product; quantity: number };
 
 export type UserCart = Record<string, CartItem>;
 
-export type SessionData = { user?: {id: string} };
+export type SessionData = { user?: { id: string } };
